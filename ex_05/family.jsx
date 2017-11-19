6d701d8a733d980485a21920f3b7d779aaad3f7c
guaracyalima <guaracyaraujolima@gmail.com>
2017-11-19 02:10:09 -0200

    Exercice 06

--- ex_05/family.jsx
+++ ex_05/family.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { childrenWithProps } from '../utils/reactUtils'
+
 //children passa tudo o que for declarado no component family para dentro do props.children
 export default props => (
   <div>
     <h1>Familia</h1>
-    { childrenWithProps(props.children, props)}
+    { props.children }
   </div>
 )
--- ex_05/index.jsx
+++ ex_05/index.jsx
@@ -3,9 +3,8 @@ import ReactDOM from 'react-dom'
 import Family from './family'
 import Member from './members'
 ReactDOM.render(
-  <Family lastName='Araujo'>
-    <Member name='guabirabaDev' />
-    <Member name='Bucetilda' />
+  <Family>
+    <Member name='guabirabaDev' lastName='full stack'/>
   </Family>,
   document.getElementById('app')
 )
