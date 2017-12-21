f7e2ba7a71f6d468d65cb4056b72b7f1d9b6bd62
guaracyalima <guaracyaraujolima@gmail.com>
2017-12-21 09:58:41 -0200

    multiples components

--- ex/component.jsx
+++ ex/component.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 
 //em aerofunctions o retunr é implicito (automatico dentro dos parenteses)
-export default () => (
-    <h1>Firist Com2ponen2t</h1>
+//props são as propriedades do componente
+
+const Primeiro = props => (
+    <h1>Primeiro componente</h1>
+    )
+
+const Segundo = props => (
+    <h1>Segundo componte</h1>
 )
+
+export { Primeiro, Segundo }
